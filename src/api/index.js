0545c97fac260f1f330338e53d44de78f4a7f6e1
d.ispaniuk <d.ispaniuk@seven-pro.com>
2021-11-09 17:31:33 +0200

    modified link to get API

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 export const getSearchWeather = (city) => {
-    return city && axios.get(`${process.env.REACT_APP_HEROKU_URL}${process.env.REACT_APP_LOCATION_URL}/v1/cities/search`, {params: {apikey: process.env.REACT_APP_API_KEY, q: city}})
+    return city && axios.get(`${process.env.REACT_APP_LOCATION_URL}/v1/cities/search`, {params: {apikey: process.env.REACT_APP_API_KEY, q: city}})
             .then(response => response.data)
 
 }
 export const getCityWeather = (cityID) => {
     localStorage.setItem('cityID', cityID)
-    return cityID && axios.get(`${process.env.REACT_APP_HEROKU_URL}${process.env.REACT_APP_FORECAST_URL}/v1/daily/5day/${cityID}`,  {params: {apikey: process.env.REACT_APP_API_KEY}}).then(response => response.data)
+    return cityID && axios.get(`${process.env.REACT_APP_FORECAST_URL}/v1/daily/5day/${cityID}`,  {params: {apikey: process.env.REACT_APP_API_KEY}}).then(response => response.data)
 }
